20489b6977f6346af2ee38c656cf5a5a2d227d1a
Nithin <60612488+nithinK-142@users.noreply.github.com>
2023-10-02 13:34:36 +0530

    Added disco mode

--- color-palette/script.js
+++ color-palette/script.js
@@ -1,9 +1,14 @@
 const colorPalette = document.getElementById("color-palette");
 const generateButton = document.getElementById("generate-button");
-const saveButton = document.getElementById("save-button");
+const discoButton = document.getElementById("disco-button");
+const stopDiscoButton = document.getElementById("stop-disco-button");
 let colors = [];
+let backgroundInterval;
+let discoInterval;
 
 generateButton.addEventListener("click", generatePalette);
+discoButton.addEventListener("click", startDisco);
+stopDiscoButton.addEventListener("click", stopDisco);
 
 function generateRandomColor() {
   const letters = "0123456789ABCDEF";
@@ -14,16 +19,21 @@ function generateRandomColor() {
   return color;
 }
 
+function generateColorBox(randomColor) {
+  const colorBox = document.createElement("div");
+  colorBox.className = "color-box";
+  colorBox.style.backgroundColor = randomColor;
+  colorBox.addEventListener("click", () => copyToClipboard(randomColor));
+  return colorBox;
+}
+
 function generatePalette() {
   colors = [];
   colorPalette.innerHTML = "";
   for (let i = 0; i < 5; i++) {
     const randomColor = generateRandomColor();
     colors.push(randomColor);
-    const colorBox = document.createElement("div");
-    colorBox.className = "color-box";
-    colorBox.style.backgroundColor = randomColor;
-    colorBox.addEventListener("click", () => copyToClipboard(randomColor));
+    const colorBox = generateColorBox(randomColor);
     colorPalette.appendChild(colorBox);
   }
 }
@@ -37,6 +47,22 @@ function copyToClipboard(text) {
   document.execCommand("copy");
   document.body.removeChild(tempInput);
   alert(`Copied: ${text}`);
+}
+
+
+async function startDisco() {
+  backgroundInterval = setInterval(generateRandomBackgroundColor, 1000);
+  discoInterval = setInterval(generatePalette, 400);
+}
+
+function stopDisco() {
+  clearInterval(backgroundInterval);
+  document.body.style.backgroundColor = '#315968';
+  clearInterval(discoInterval);
+}
+
+function generateRandomBackgroundColor() {
+  document.body.style.backgroundColor = generateRandomColor();
 }
 
 generatePalette();
